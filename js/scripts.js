cce6019d5482b8b5ea08c7b9c844bd4e6e42aa6c
hussein18149 <husseinkatana17@gmail.com>
2019-05-14 23:07:59 +0300

    remove

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,6 @@ image[5]= 'images/img10.png';
 
   document.getElementById("image2").src=image[temp];
 }
-var image
 
 
 var player1="";
@@ -88,7 +87,7 @@ class Player {
     this.totalscore = 0;
     this.playerName = "";
   }
-}
+};
 // *************USER INTERFACE*********************************************
 $(document).ready(function() {
 
@@ -106,8 +105,37 @@ $(document).ready(function() {
     player2.playerName=player2Name;
 
   });
-  
-});
+
+  $("button#player1-roll").click(function(event){
+    player1.roll = throwdice();
+    $("#roll-1").text(player1.roll);
+    player1.rollone();
+    $("#round-1").text(player1.tempscore);
+  });
+ 
+  $("button#player2-roll").click(function(event){
+    player2.roll = throwdice();
+    $("#roll-2").text(player2.roll);
+    player2.rollone();
+    $("#round-2").text(player2.tempscore);
+  });
+
+  $("button#player1-hold").click(function(event){
+    player1.hold();
+    $("#total-1").text(player1.totalscore);
+    $("#round-1").empty();
+    $("#roll-1").empty();
+    player1.winnerHere();
+  });
+ 
+  $("button#player2-hold").click(function(event){
+    player2.hold();
+    $("#total-2").text(player2.totalscore);
+    $("#round-2").empty();
+    $("#roll-2").empty();
+    player2.winnerHere();
+  });
+ });
 
 
 
